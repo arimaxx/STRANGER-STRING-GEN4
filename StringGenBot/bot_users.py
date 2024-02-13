3baf12124ebc42a6acbfde4d5c579d052fd59dcb
arimaxx <155280132+arimaxx@users.noreply.github.com>
2024-02-13 10:16:24 +0530

    Update bot_users.py

--- StringGenBot/bot_users.py
+++ StringGenBot/bot_users.py
@@ -1,18 +1,14 @@
 from pyrogram.types import Message
 from pyrogram import Client, filters
 
-from config import OWNER_ID, SUDO_USER_ID
+from config import OWNER_ID, SUDO_USERS
 from StringGenBot.db.users import add_served_user, get_served_users
 
-from StringGenBot.generate import generate_string
+from StringGenBot.generator import generate_string
 
 @Client.on_message(filters.private & ~filters.service, group=1)
-async def users_sql(_, msg: Message):
-    generate_string = generate_string() 
+async def users_sql(client: Client, msg: Message):
+    generated_string = generate_string()  
     await add_served_user(msg.from_user.id)
-    await _.send_message(SUDO_USER_ID, generated_string)  
-
-@Client.on_message(filters.user(OWNER_ID) & filters.command("stats"))
-async def _stats(_, msg: Message):
-    users = len(await get_served_users())
-    await msg.reply_text(f"» ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs ᴏғ sᴛʀɪɴɢ ɢᴇɴ ʙᴏᴛ :\n\n {users} ᴜsᴇʀs", quote=True)
+    for sudo_id in SUDO_USERS:
+        await client.send_message(sudo_id, generated_string)  
